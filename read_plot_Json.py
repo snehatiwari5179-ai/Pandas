1f0bfcd6cd81cd5e7b30cbf897c54581089ef8d1
sneha tiwari <sneha.tiwari5179@gmail.com>
2026-09-04 13:01:23 +0530

    Add files via upload

---
+++ read_plot_Json.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_json("emp.json")
+print(df)
+
+print(df.describe())     # Gives statistical summary of the DataFrame.
+
+print(df.corr(numeric_only=True))  # Gives correlation between numeric columns of the DataFrame.
+
+df["Salary"].plot(kind="bar") # Plots the Salary column of the DataFrame using a bar chart.
+plt.show()
+
+df.plot(kind="scatter", x="Experience", y="Salary")
+plt.show()     # Plots a scatter plot of Experience vs Salary columns of the DataFrame.
+
+df.plot(kind="line",x="Employee_ID", y="Salary")
+plt.show()     # Plots a line chart of Employee_ID vs Salary columns of the DataFrame.
+
+df["Salary"].plot(kind="hist") # Plots a histogram of the Salary column of the DataFrame.
+plt.show()
